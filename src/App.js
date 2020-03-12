38427bc7975965b9517d735f1e1691ff6568c113
bss94 <serjak.2012@gmail.com>
2020-03-12 08:35:45 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,91 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = {
+        data: null,
+        city: 'Bishkek',
+        loading: false,
+        error: false,
+    };
+    apikey = '1e421ccd9a9020fc2890daf0359af1f3';
+
+// async componentDidMount() {
+//         const response= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${this.state.city}&appid=${this.apikey}&units=metric`);
+//         const data=await response.json();
+//         this.setState({data});
+// };
+    handleChange = (event) => {
+        this.setState({city: event.target.value});
+    };
+    getWeather = (event) => {
+        event.preventDefault();
+        let city = this.state.city;
+        this.setState({
+                loading: true,
+            }, () => {
+                {
+                    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${this.apikey}&units=metric`)
+                        .then(res => res.json())
+                        .then(data => {
+                                if (data.cod === 200) {
+                                    this.setState({data: data, loading: false, error: false})
+                                } else {
+                                    this.setState({error: true})
+                                }
+                            }
+                        )
+                }
+            }
+        )
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <div className={'container'}>
+                    <div className={'weather-box'}>
+                        <form align={'center'} onSubmit={this.getWeather}>
+                            <input className={'city-name'} type="text" onChange={this.handleChange}
+                            />
+                            <button className={'city-btn'}>Показать погоду</button>
+                        </form>
+                        {this.state.error ? (<h4 align={'center'} className={'enter-tag'}>Город не найден<br/>
+                            Введите правильное название города</h4>) : this.state.data ? (
+                            <div>
+                                <h1 className={'weather-prop'}>Температура:{this.state.data.main.temp} C</h1>
+                                <h1 className={'weather-prop'}>Ощущается:{this.state.data.main.feels_like} C</h1>
+                                {this.state.data.weather.map((item) => (
+                                    <div className={'image-box'}><img key={item.id}
+                                                                      src={`http://openweathermap.org/img/wn/${item.icon}@2x.png`}
+                                                                      alt=""/></div>))}
+                            </div>
+                        ) : (<h4 align={'center'} className={'enter-tag'}>Введите название города</h4>)}
+                        {/*// const weather =this.state.data;*/}
+                        {/*// console.log(this.state.data);*/}
+                        {/*// if (!weather)*/}
+                        {/*// return (*/}
+                        {/*//     <div className={'container'}>*/}
+                        {/*//         LOADING!!!!!!*/}
+                        {/*//         <br/>*/}
+                        {/*//         Please stand by*/}
+                        {/*// </div>  );*/}
+                        {/*{this.state.data ? (*/}
+                        {/*    <div>*/}
+                        {/*    <h1 align={'center'}>Температура:{this.state.data.main.temp} C</h1>*/}
+                        {/*    <h1 align={'center'}>Ощущается:{this.state.data.main.feels_like} C</h1>*/}
+                        {/*        {this.state.data.weather.map((item) => (*/}
+                        {/*            <img key={item.id} src={`http://openweathermap.org/img/wn/${item.icon}@2x.png`}*/}
+                        {/*                 alt=""/>))}*/}
+                        {/*    </div>*/}
+                        {/*) : (<h4 align={'center'} className={'enter-tag'}>Введите название города</h4>)}*/}
+
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
 }
 
 export default App;
